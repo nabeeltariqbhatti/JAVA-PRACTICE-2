80ff47d8567827d1ed936f5d97bee62e067d8df0
nabeeltaariq <nabeeltaariq@gmail.com>
2021-04-28 08:18:58 -0700

    done

--- src/practice/nabeel/StrinReverse.java
+++ src/practice/nabeel/StrinReverse.java
@@ -1,10 +1,37 @@
 package practice.nabeel;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class StrinReverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		   Scanner sc=new Scanner(System.in);
+			
+			System.out.print("Enter the string to reverse");
+		             String	 line=sc.nextLine();
+		             
+		            System.out.print(line + " reverse as "  );
+		            reverseString(line);
+	}
+	
+	public static void reverseString(String line) {
+	String[] words=line.split(" ");
+	String newWords="";
+	
+//	for(int i=0; i<words.length;i++) {
+//		for(int j= words[i].length(); j>=0;--j) {
+//			System.out.print(words[j]);
+//		}
+//	}
+		for(String word:words) {
+			StringBuilder sb=new StringBuilder(word);
+			newWords=newWords+sb.reverse() + " ";
+		}
+		
+		System.out.print(newWords);
+		
 	}
 
 }
